bf6845b5042becd2af8be0e7522f2af01595c665
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-03-22 23:17:12 +0530

    feat : shadcn integration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -7,6 +8,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+
   plugins: [
     vue(),
     vueJsx(),
