e1ed168bc848c751ddcb0ad8e8e290cee0099289
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-03-26 14:51:12 +0530

    fix : code refactored

--- frontend/src/stores/authentication.ts
+++ frontend/src/stores/authentication.ts
@@ -1,78 +1,68 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import AxiosInstance from "@/config/axiosConfig"
 import { jwtDecode } from 'jwt-decode'
 
-interface DecodedToken {
-    name: string;
-    email: string;
-    role: string;
-}
 // types
-import type { user_state } from "@/types/user"
+import type { user_state } from '@/types/user'
+import type { DecodedToken } from '@/types/miscellaneous'
 
 export const useAuthStore = defineStore('auth', {
-    state: (): user_state => {
-        return {
-            session: '',
-            name: '',
-            email: '',
-            role: ''
-        }
+  state: (): user_state => {
+    return {
+      session: '',
+      name: '',
+      email: '',
+      role: '',
+    }
+  },
+  getters: {
+    is_authenticated: (state) => {
+      return state.session !== ''
     },
-    getters: {
-        is_authenticated: (state) => {
-            return state.session !== ''
-        },
-        is_admin: (state) => {
-            return state.role === 'admin'
-        },
-        is_student: (state) => {
-            return state.role === 'student'
-        },
-        is_educator: (state) => {
-            return state.role === 'educator'
-        },
-        get_user: (state) => {
-            return state
-        }
-
+    is_admin: (state) => {
+      return state.role === 'admin'
     },
-    actions: {
-        login_user(user_data: user_state) {
-            if (user_data.session) this.session = user_data.session
-            if (user_data.name) this.name = user_data.name
-            if (user_data.role) this.role = user_data.role
-        },
-
-        async init(cookie: string): Promise<void> {
-            try {
-                const token = cookie.split('=')[1] // Get token value from cookie
-                const decoded_token = jwtDecode<DecodedToken>(token)
-
-                if (decoded_token) {
-                    this.login_user({
-                        session: token,
-                        name: decoded_token.name,
-                        email: decoded_token.email,
-                        role: decoded_token.role
-                    })
-                }
-
-            } catch (error) {
-                console.error('Failed to initialize session:', error)
-            }
-        },
-
-        logout() {
-            // Remove the cookie by setting its expiration to past date
-            document.cookie = "user_cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-            this.session = ''
-            this.name = ''
-            this.email = ''
-            this.role = ''
-        }
-
-
+    is_student: (state) => {
+      return state.role === 'student'
     },
+    is_educator: (state) => {
+      return state.role === 'educator'
+    },
+    get_user: (state) => {
+      return state
+    },
+  },
+  actions: {
+    login_user(user_data: user_state) {
+      if (user_data.session) this.session = user_data.session
+      if (user_data.name) this.name = user_data.name
+      if (user_data.role) this.role = user_data.role
+      if (user_data.email) this.email = user_data.email
+    },
+
+    async init(cookie: string): Promise<void> {
+      try {
+        const token = cookie.split('=')[1] // Get token value from cookie
+        const decoded_token = jwtDecode<DecodedToken>(token)
+        console.log('Decoded token:', decoded_token.email)
+
+        this.login_user({
+          session: token,
+          name: decoded_token.name,
+          email: decoded_token.email,
+          role: decoded_token.role,
+        })
+      } catch (error) {
+        console.error('Failed to initialize session:', error)
+      }
+    },
+
+    logout() {
+      // Remove the cookie by setting its expiration to past date
+      document.cookie = 'user_cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+      this.session = ''
+      this.name = ''
+      this.email = ''
+      this.role = ''
+    },
+  },
 })
