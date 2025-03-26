3a45c3626889cedf1f98ac0221986144e06c1a31
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-03-26 14:51:23 +0530

    Create miscellaneous.ts

---
+++ frontend/src/types/miscellaneous.ts
@@ -0,0 +1,13 @@
+// Objective: Define the types for the miscellaneous data
+
+export interface navItem {
+  nav: string
+  icon: string
+  link: string
+}
+
+export interface DecodedToken {
+  name: string
+  email: string
+  role: string
+}
