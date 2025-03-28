cbe2e4ca7120ff492c5522914588fa50f7b279b6
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-03-29 02:22:36 +0530

    feat : initialized firestore database

--- frontend/src/config/firebaseConfig.ts
+++ frontend/src/config/firebaseConfig.ts
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from 'firebase/analytics'
+import { getFirestore } from 'firebase/firestore'
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,12 +16,13 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
   appId: import.meta.env.VITE_appId,
-  measurementId: import.meta.env.VITE_measurementId
-};
+  measurementId: import.meta.env.VITE_measurementId,
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
+// const analytics = getAnalytics(app);
 
 // Export auth instance
 export const auth = getAuth(app)
