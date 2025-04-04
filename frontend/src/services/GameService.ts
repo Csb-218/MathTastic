a93e84809bff53ba44d58b6e5052b0368ff0aee7
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-04-05 00:49:50 +0530

    feat : added two new services
    addActivityToGame
    createActivity

--- frontend/src/services/GameService.ts
+++ frontend/src/services/GameService.ts
@@ -1,4 +1,5 @@
 import AxiosInstance from '@/config/axiosConfig'
+import type { Activity } from '@/types/activity'
 
 // get all games
 export async function getAllGames() {
@@ -13,6 +14,31 @@ export async function getAllGames() {
 export async function getGameById(id: string) {
   try {
     const response = await AxiosInstance.get(`/games/${id}`)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+// add activity to game
+export async function addActivityToGame(id: string, activity_id: Activity) {
+  try {
+    const requestBody = {
+      activity_ids: [activity_id],
+    }
+
+    const response = await AxiosInstance.patch(`/games/${id}/activities`, requestBody)
+
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+// create activity
+export async function createActivity(activity: Activity) {
+  try {
+    const response = await AxiosInstance.post('/activities/create', activity)
     return response.data
   } catch (error) {
     console.error(error)
