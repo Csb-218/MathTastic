7566fed77911927f17050219d4c4736960c1994b
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-04-06 02:23:43 +0530

    feat : added touch action controllers

--- frontend/src/views/Student/games/BalanceScale.vue
+++ frontend/src/views/Student/games/BalanceScale.vue
@@ -1,130 +1,3 @@
-<template>
-
-  <div ref="gameContainer"
-    class="game-container  bg-[url(@/assets/backgrounds/rainbow_landscape.jpg)] bg-cover text-center p-5 bg-[#f5f5d5] font-sans relative ">
-
-    <!-- Loading state -->
-    <div v-if="loading" class="absolute inset-0 flex items-center justify-center bg-white/50">
-      <div class="loading-spinner">Loading...</div>
-    </div>
-
-    <!-- Error state -->
-    <div v-else-if="error" class="absolute inset-0 flex items-center justify-center bg-white/50">
-      <div class="text-red-500">{{ error }}</div>
-    </div>
-
-    <!-- Mobile View with Play Button -->
-    <OverLay v-else-if="(isMobileView && !isPlaying) || isCompleted" :start-game="startGame" :restartGame="restartGame"
-      :container="gameContainer" :isCompleted="isCompleted" />
-
-    <!-- Game content -->
-    <template v-else>
-
-      <!-- Exit game button -->
-      <button @click="exitGame" class="absolute top-4 right-4 p-2 text-white  " title="Exit fullscreen">
-        <img :src="exit" alt="exit" class="w-10 h-10" />
-      </button>
-      <!-- Minimize -->
-      <button @click="toggleFullscreen(gameContainer)"
-        class="absolute top-4 left-4 p-1 text-white bg-slate-800 rounded  " title="Minimize">
-        <Minimize class="w-10 h-10" />
-      </button>
-
-      <!-- Level board -->
-      <span class="absolute top-4 left-1/2 -translate-x-1/2 p-2 text-white">
-        <img :src="level_board" alt="level_board" class="h-20" />
-        <p class="z-50 absolute top-1/3 left-1/2 -translate-x-1/2 font-bubblegum text-2xl ">Level {{ currentLevel }}
-        </p>
-      </span>
-
-      <!-- balance status -->
-      <div v-if="showMessage"
-        class="absolute -translate-x-1/2 top-1/4 left-1/2 p-4 text-yellow-400 bg-slate-900/80 rounded-lg transform transition-all duration-300 ease-in-out"
-        :class="{ 'scale-100 opacity-100': showMessage, 'scale-95 opacity-0': !showMessage }">
-        <p class="font-bubblegum text-2xl">{{ balanceStatus }}</p>
-      </div>
-
-      <!-- Balance scale -->
-      <div class=" flex justify-center my-5 absolute h-2/4 bottom-0 left-1/2 -translate-x-1/2"
-        :class="isFullScreen ? ' w-full' : ' w-1/2'">
-        <!-- Fixed vertical stand -->
-        <img :src="stand" alt="stand" class="stand h-full drop-shadow-lg" />
-        <!-- Rotating beam and baskets -->
-        <div class="beam w-[70%] sm:w-[60%] md:w-[60%] lg:w-1/2 cursor-grabbing"
-          :style="{ transform: `rotate(${tiltAngle}deg)` }">
-          <img :src="nut" alt="nut"
-            class="absolute top-[10px] left-1/2 -translate-x-1/2 w-[30px] h-[30px] z-50 bg-amber-300 rounded-full border-0">
-          <!-- Horizontal beam -->
-          <div class="beam-bar absolute top-[20px] left-0 w-full h-[10px] bg-[#999] rounded-[5px]"></div>
-          <div
-            class="pan left-pan absolute top-[40px] -left-16 w-[150px] h-[150px] flex flex-wrap items-end py-4 justify-center"
-            @drop="drop($event, 'left')" @dragover="allowDrop">
-            <div v-for="obj in leftPanObjects" :key="obj.id" class="object-container">
-              <div :id="obj.id" class="game-object w-[30px] h-[30px] rounded-full " draggable="true"
-                @dragstart="drag($event, obj)">
-                <img v-if="obj.image" :src="obj.image" :alt="obj.type" class="w-full h-full object-contain" />
-              </div>
-            </div>
-            <p class="absolute -bottom-7 font-bubblegum text-white text-lg">{{ leftWeight }}</p>
-          </div>
-          <div
-            class="pan right-pan absolute top-[40px] -right-16 w-[150px] h-[150px] flex flex-wrap items-end py-4 justify-center"
-            @drop="drop($event, 'right')" @dragover="allowDrop">
-            <div v-for="obj in rightPanObjects" :key="obj.id" class="object-container">
-              <div :id="obj.id" class="game-object w-[30px] h-[30px] rounded-full " draggable="true"
-                @dragstart="drag($event, obj)">
-                <img v-if="obj.image" :src="obj.image" :alt="obj.type" class="w-full h-full object-contain" />
-              </div>
-
-            </div>
-            <p class="absolute -bottom-7 font-bubblegum text-white text-lg">{{ rightWeight }}</p>
-          </div>
-        </div>
-      </div>
-
-      <!-- weighing objects -->
-      <div class="weights my-5 absolute left-1/2 -translate-x-1/2" :class="isFullScreen ? 'bottom-10' : 'bottom-4'">
-        <div class="flex justify-center gap-2 ">
-          <div v-for="obj in availableObjects" :key="obj.id"
-            class="object-container flex flex-col items-center m-1 cursor-grabbing">
-            <div :id="obj.id" class="game-object w-[30px] h-[30px] rounded-full " draggable="true"
-              @dragstart="drag($event, obj)">
-              <img v-if="obj.image" :src="obj.image" :alt="obj.type" class="w-full h-full object-contain" />
-              <p class="text-white font-bubblegum">{{ obj.weight }}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- game control buttons -->
-
-      <!-- left controls -->
-      <div class="flex flex-col justify-center gap-1 absolute left-5 bottom-14 ">
-        <button @click="resetGame" class="p-2 text-white sm:left-4 sm:bottom-10 ">
-          <img :src="reload" alt="reload" class="w-10 h-10" />
-        </button>
-        <button @click="resetGame" class="p-2 text-white sm:left-4 sm:bottom-10 ">
-          <img :src="music" alt="reload" class="w-10 h-10" />
-        </button>
-      </div>
-
-      <!-- right controls -->
-      <div class="flex flex-col justify-center gap-1 absolute right-5 bottom-14 ">
-        <button @click="resetGame" class=" p-2 text-white sm:left-4 sm:bottom-10 ">
-          <img :src="more_games" alt="reload" class="w-10 h-10" />
-        </button>
-        <button @click="resetGame" class=" p-2 text-white sm:left-4 sm:bottom-10 ">
-          <img :src="leaderboard" alt="reload" class="w-10 h-10" />
-        </button>
-
-      </div>
-
-
-    </template>
-
-  </div>
-</template>
-
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted, watch, computed } from 'vue'
 import { useEventListener } from '@vueuse/core'
@@ -189,7 +62,10 @@ const {
   drop,
   drag,
   allowDrop,
-  toggleFullscreen
+  toggleFullscreen,
+  handleTouchStart,
+  handleTouchMove,
+  handleTouchEnd
 } = useBalanceScaleStore()
 
 const { uid } = storeToRefs(useAuthStore())
@@ -296,6 +172,138 @@ onUnmounted(() => {
 
 </script>
 
+<template>
+
+  <div ref="gameContainer"
+    class="game-container  bg-[url(@/assets/backgrounds/rainbow_landscape.jpg)] bg-cover text-center p-5 bg-[#f5f5d5] font-sans relative ">
+
+    <!-- Loading state -->
+    <div v-if="loading" class="absolute inset-0 flex items-center justify-center bg-white/50">
+      <div class="loading-spinner">Loading...</div>
+    </div>
+
+    <!-- Error state -->
+    <div v-else-if="error" class="absolute inset-0 flex items-center justify-center bg-white/50">
+      <div class="text-red-500">{{ error }}</div>
+    </div>
+
+    <!-- Mobile View with Play Button -->
+    <OverLay v-else-if="(isMobileView && !isPlaying) || isCompleted" :start-game="startGame" :restartGame="restartGame"
+      :container="gameContainer" :isCompleted="isCompleted" />
+
+    <!-- Game content -->
+    <template v-else>
+
+      <!-- Exit game button -->
+      <button @click="exitGame" class="absolute top-4 right-4 p-2 text-white  " title="Exit fullscreen">
+        <img :src="exit" alt="exit" class="w-10 h-10" />
+      </button>
+      <!-- Minimize -->
+      <button @click="toggleFullscreen(gameContainer)"
+        class="absolute top-4 left-4 p-1 text-white bg-slate-800 rounded  " title="Minimize">
+        <Minimize class="w-10 h-10" />
+      </button>
+
+      <!-- Level board -->
+      <span class="absolute top-4 left-1/2 -translate-x-1/2 p-2 text-white">
+        <img :src="level_board" alt="level_board" class="h-20" />
+        <p class="z-50 absolute top-1/3 left-1/2 -translate-x-1/2 font-bubblegum text-2xl ">Level {{ currentLevel }}
+        </p>
+      </span>
+
+      <!-- balance status -->
+      <div v-if="showMessage"
+        class="absolute -translate-x-1/2 top-1/4 left-1/2 p-4 text-yellow-400 bg-slate-900/80 rounded-lg transform transition-all duration-300 ease-in-out"
+        :class="{ 'scale-100 opacity-100': showMessage, 'scale-95 opacity-0': !showMessage }">
+        <p class="font-bubblegum text-2xl">{{ balanceStatus }}</p>
+      </div>
+
+      <!-- Balance scale -->
+      <div class=" flex justify-center my-5 absolute h-2/4 bottom-0 left-1/2 -translate-x-1/2"
+        :class="isFullScreen ? ' w-full' : ' w-1/2'">
+        <!-- Fixed vertical stand -->
+        <img :src="stand" alt="stand" class="stand h-full drop-shadow-lg" />
+        <!-- Rotating beam and baskets -->
+        <div class="beam w-[70%] sm:w-[60%] md:w-[60%] lg:w-1/2 cursor-grabbing"
+          :style="{ transform: `rotate(${tiltAngle}deg)` }">
+          <img :src="nut" alt="nut"
+            class="absolute top-[10px] left-1/2 -translate-x-1/2 w-[30px] h-[30px] z-50 bg-amber-300 rounded-full border-0">
+          <!-- Horizontal beam -->
+          <div class="beam-bar absolute top-[20px] left-0 w-full h-[10px] bg-[#999] rounded-[5px]"></div>
+          <div
+            class="pan left-pan absolute top-[40px] -left-16 w-[150px] h-[150px] flex flex-wrap items-end py-4 justify-center"
+            @drop="drop($event, 'left')" @dragover="allowDrop">
+            <div v-for="obj in leftPanObjects" :key="obj.id" class="object-container">
+              <div :id="obj.id" class="game-object w-[30px] h-[30px] rounded-full " draggable="true"
+                @dragstart="drag($event, obj)" @touchstart="(e: TouchEvent) => handleTouchStart(e, obj)"
+                @touchmove="(e: TouchEvent) => handleTouchMove(e)" @touchend="handleTouchEnd">
+                <img v-if="obj.image" :src="obj.image" :alt="obj.type" class="w-full h-full object-contain" />
+              </div>
+            </div>
+            <p class="absolute -bottom-7 font-bubblegum text-white text-lg">{{ leftWeight }}</p>
+          </div>
+          <div
+            class="pan right-pan absolute top-[40px] -right-16 w-[150px] h-[150px] flex flex-wrap items-end py-4 justify-center"
+            @drop="drop($event, 'right')" @dragover="allowDrop">
+            <div v-for="obj in rightPanObjects" :key="obj.id" class="object-container">
+              <div :id="obj.id" class="game-object w-[30px] h-[30px] rounded-full " draggable="true"
+                @dragstart="drag($event, obj)" @touchstart="(e: TouchEvent) => handleTouchStart(e, obj)"
+                @touchmove="(e: TouchEvent) => handleTouchMove(e)" @touchend="handleTouchEnd">
+                <img v-if="obj.image" :src="obj.image" :alt="obj.type" class="w-full h-full object-contain" />
+              </div>
+
+            </div>
+            <p class="absolute -bottom-7 font-bubblegum text-white text-lg">{{ rightWeight }}</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- weighing objects -->
+      <div class="weights my-5 absolute left-1/2 -translate-x-1/2" :class="isFullScreen ? 'bottom-10' : 'bottom-4'">
+        <div class="flex justify-center gap-2 ">
+          <div v-for="obj in availableObjects" :key="obj.id"
+            class="object-container flex flex-col items-center m-1 cursor-grabbing">
+            <div :id="obj.id" class="game-object w-[30px] h-[30px] rounded-full " draggable="true"
+              @dragstart="drag($event, obj)" @touchstart="(e: TouchEvent) => handleTouchStart(e, obj)"
+              @touchmove="(e: TouchEvent) => handleTouchMove(e)" @touchend="handleTouchEnd">
+              <img v-if="obj.image" :src="obj.image" :alt="obj.type" class="w-full h-full object-contain" />
+              <p class="text-white font-bubblegum">{{ obj.weight }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- game control buttons -->
+
+      <!-- left controls -->
+      <div class="flex flex-col justify-center gap-1 absolute left-5 bottom-14 ">
+        <button @click="resetGame" class="p-2 text-white sm:left-4 sm:bottom-10 ">
+          <img :src="reload" alt="reload" class="w-10 h-10" />
+        </button>
+        <button @click="resetGame" class="p-2 text-white sm:left-4 sm:bottom-10 ">
+          <img :src="music" alt="reload" class="w-10 h-10" />
+        </button>
+      </div>
+
+      <!-- right controls -->
+      <div class="flex flex-col justify-center gap-1 absolute right-5 bottom-14 ">
+        <button @click="resetGame" class=" p-2 text-white sm:left-4 sm:bottom-10 ">
+          <img :src="more_games" alt="reload" class="w-10 h-10" />
+        </button>
+        <button @click="resetGame" class=" p-2 text-white sm:left-4 sm:bottom-10 ">
+          <img :src="leaderboard" alt="reload" class="w-10 h-10" />
+        </button>
+
+      </div>
+
+
+    </template>
+
+  </div>
+</template>
+
+
+
 <style scoped>
 .beam {
   position: relative;
@@ -379,9 +387,6 @@ onUnmounted(() => {
 
 
 .game-container:fullscreen {
-  /* background-image: url("@/assets/backgrounds/rainbow_landscape.jpg");
-  background-size: cover; */
-  /* display: flex; */
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -390,6 +395,8 @@ onUnmounted(() => {
 
 .game-object {
   transition: transform 0.2s ease;
+  touch-action: none;
+  user-select: none;
 }
 
 .game-object:hover {
@@ -434,5 +441,26 @@ onUnmounted(() => {
 
 .scale-win {
   animation: bounce 0.5s ease-in-out;
+}
+
+.game-object.dragging {
+  opacity: 0.7;
+  transform: scale(1.1);
+  pointer-events: none;
+  z-index: 1000;
+}
+
+.pan.drop-target {
+  background-color: rgba(255, 255, 255, 0.2);
+  transition: all 0.2s ease;
+}
+
+#dragging-clone {
+  will-change: transform;
+  touch-action: none;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  user-select: none;
+  image-rendering: -webkit-optimize-contrast;
 }
 </style>
