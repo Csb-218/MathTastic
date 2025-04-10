6e1de023b82d7b56f426fddf6e87e4e1105239df
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-04-11 03:15:54 +0530

    feat : added gameObjects to exports

--- frontend/src/assets/game/index.ts
+++ frontend/src/assets/game/index.ts
@@ -6,5 +6,36 @@ import leaderboard from './leaderboard.png'
 import more_games from './more_games.png'
 import music from './music.png'
 import level_board from './level_board.png'
+import points_board from './points_board.png'
+import congratulations_character from './congratulations_character.gif'
 
-export { nut, stand, reload, exit, leaderboard, more_games, music, level_board }
+//  game object icons
+import appleIcon from './apple.svg'
+import bananaIcon from './banana.svg'
+import orangeIcon from './orange.svg'
+import mushroomIcon from './mushroom.svg'
+import beetrootIcon from './beetroot.svg'
+import watermelonIcon from './watermelon.svg'
+import strawberryIcon from './strawberry.svg'
+import grapesIcon from './grapes.svg'
+
+export {
+  nut,
+  stand,
+  reload,
+  exit,
+  leaderboard,
+  more_games,
+  music,
+  level_board,
+  appleIcon,
+  bananaIcon,
+  orangeIcon,
+  mushroomIcon,
+  beetrootIcon,
+  watermelonIcon,
+  strawberryIcon,
+  grapesIcon,
+  points_board,
+  congratulations_character,
+}
