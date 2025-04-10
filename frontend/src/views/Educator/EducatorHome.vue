13bb1cab1fb1c8d323a014471a12fdf533cd20a7
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-04-11 02:53:08 +0530

    Create EducatorHome.vue

---
+++ frontend/src/views/Educator/EducatorHome.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import NavbarEducator from '@/components/blocks/navigation/NavbarEducator.vue'
+import { RouterView } from 'vue-router'
+
+
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-gradient-to-b py-10 from-amber-50 to-white">
+    <NavbarEducator />
+    <RouterView />
+  </div>
+</template>
