6b7666937b2d64111997c81e75f5cae2b97dc6d8
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-04-13 00:09:10 +0530

    feat : added checkAuthentication service

--- frontend/src/services/AuthService.ts
+++ frontend/src/services/AuthService.ts
@@ -1,31 +1,43 @@
-import AxiosInstance from "@/config/axiosConfig";
-import type { user, login } from "@/types/user"
+import AxiosInstance from '@/config/axiosConfig'
+import type { user, login } from '@/types/user'
 
 export async function SignUser(user: user, token: string) {
-   console.log(user, token)
-   try {
-      const response = await AxiosInstance.post('/users/register', user, {
-         headers: {
-            'Authorization': `Bearer ${token}`,
-         }
-      })
-      return response
-   } catch (err) {
-      console.log(err)
-   }
-
+  console.log(user, token)
+  try {
+    const response = await AxiosInstance.post('/users/register', user, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return response
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function LoginUser(user: login, token: string) {
-   try {
-      const response = await AxiosInstance.post('/users/login', user, {
-         headers: {
-            'Authorization': `Bearer ${token}`,
-         }
-      })
-      return response
-   } catch (e) {
-      console.error(e)
-   }
+  try {
+    const response = await AxiosInstance.post('/users/login', user, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    return response
+  } catch (e) {
+    console.error(e)
+  }
 }
 
+export async function checkAuthentication(cookie: string) {
+  try {
+    const response = await AxiosInstance.get('/users/check-auth', {
+      headers: {
+        user_cookie: cookie,
+      },
+    })
+    return response
+  } catch (e) {
+    console.error(e)
+  }
+}
